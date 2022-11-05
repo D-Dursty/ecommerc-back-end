9b28015a0dc8b83d4ee8abce7c9bad6438720e55
D-Dursty <devondahlia@gmail.com>
2022-11-05 14:28:38 -0700

    bug fixes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+// const seeds = require('./seeds')
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
